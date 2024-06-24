45febcc1cf205c6945816b9881827ac17d4801bb
troh <dhxofla91@gmail.com>
2024-06-24 21:06:23 +0900

    init

---
+++ src/main/java/com/example/kafkastreamsdslpractice/KafkaStreamsDslPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamsdslpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsDslPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsDslPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkastreamsdslpractice/KafkaStreamsDslPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamsdslpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsDslPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
